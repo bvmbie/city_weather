ccf5e1c08a8aba8d74c38f332a65fb228fc536ac
Henna Vahatiitto <hnnamv@gmail.com>
2020-02-11 11:51:42 +0000

    first commit

---
+++ lib/city_weather.rb
@@ -0,0 +1,6 @@
+require "city_weather/version"
+
+module CityWeather
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/city_weather/version.rb
@@ -0,0 +1,3 @@
+module CityWeather
+  VERSION = "0.1.0"
+end
