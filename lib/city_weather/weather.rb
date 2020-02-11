ce9c5ac99a63c3e4edfc22fbee5414dc7aedba41
Henna Vahatiitto <hnnamv@gmail.com>
2020-02-11 18:44:00 +0000

    changes

--- lib/city_weather/weather.rb
+++ lib/city_weather/weather.rb
@@ -1,13 +1,17 @@
 class CityWeather::Weather
 
-  attr_accessor :name, :temperature
+  attr_accessor :location, :temperature
 
   def self.scrape
     doc = Nokogiri::HTML(open("https://www.timeanddate.com/weather/?continent=europe&low=c"))
-    name = doc.search("div.tb-scroll a").text.split(/(?=[A-BD-LO-Z])|(?=North)|(?=Chișinău)|(?=Copenhagen)|(?=Madrid)|(?=Minsk)|(?=Monaco)|(?=Moscow)|(?<=Yerevan)/)
-    temperature = doc.search("td.rbi").text.split(/(?<=C)/)
+    
+    weather = self.new
+    weather.location = doc.search("div.tb-scroll a").text.split(/(?=[A-BD-LO-Z])|(?=North)|(?=Chișinău)|(?=Copenhagen)|(?=Madrid)|(?=Minsk)|(?=Monaco)|(?=Moscow)|(?<=Yerevan)/)
+    weather.temperature = doc.search("td.rbi").text.split(/(?<=C)/)
+    
+    
   end
   
-    # continue 54 here
+    # continue 59 here
 
 end
