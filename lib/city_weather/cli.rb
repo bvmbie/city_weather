1d9494cf15288818997ecb95d5978aae420452bd
Henna Vahatiitto <hnnamv@gmail.com>
2020-02-13 13:40:54 +0000

    done-ish

--- lib/city_weather/cli.rb
+++ lib/city_weather/cli.rb
@@ -12,25 +12,31 @@ class CityWeather::CLI
     cities = CityWeather::City.all
     cities.each.with_index(1) {|city, i| puts "#{i}. #{city.name.join}"}
     puts " "
-    more_info
   end
   
-  def more_info
+   def more_info
     input = nil
-    
-    puts "Which city's weather would you like to see?"
-    puts "To return to the main list type list or type exit to leave the program."
-    input = gets.strip.downcase 
-    
-    if input.to_i > 0
-      selected =  CityWeather::City.find_by_index(input.to_i - 1)
-      puts " "
-      puts "The temperature in #{selected.name.join} is currently #{selected.temperature.join}"
-      puts " "
-    elsif input == "list"
-      list_cities
-    end
-  end 
+
+    while input != "exit"
+      puts "Which city's weather would you like to see?"
+      puts "To return to the main list type list or type exit to leave the program."
+      input = gets.strip
+
+      if input.to_i>0
+        selected =  CityWeather::City.find_by_index(input.to_i - 1)
+        if selected == nil
+          puts "Not sure what you meant. Please try again."
+        else
+          puts " "
+          puts "The temperature in #{selected.name.join} is currently #{selected.temperature.join}"
+          puts " "
+        end
+      elsif input == "list"
+        list_cities
+      end
+    end 
+
+  end
 
   def goodbye
     puts " "
